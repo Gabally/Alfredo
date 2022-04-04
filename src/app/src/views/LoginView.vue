305642aca344fc11027ad13384c1d3a7911bbced
Gabriele Morisano <gaballyno@gmail.com>
2022-04-04 18:06:20 +0200

    added loading state to login page

--- src/app/src/views/LoginView.vue
+++ src/app/src/views/LoginView.vue
@@ -1,13 +1,28 @@
 <template>
   <div class="f-center">
     <form @submit.prevent="login()" class="form">
-        <h2>Alfredo authentication</h2>
-        <input v-model="username" required placeholder="Username" class="txt-field" type="text">
-        <input v-model="password" required placeholder="Password" class="txt-field" type="password">
-        <div class="error" v-if="error"> 
-          {{ error }}
-        </div>
-        <button class="btn b-ok">Login</button>
+      <h2>Alfredo authentication</h2>
+      <input
+        v-model="username"
+        required
+        placeholder="Username"
+        class="txt-field"
+        type="text"
+      />
+      <input
+        v-model="password"
+        required
+        placeholder="Password"
+        class="txt-field"
+        type="password"
+      />
+      <div class="error" v-if="error">
+        {{ error }}
+      </div>
+      <div v-if="loading">Loggin in...</div>
+      <button v-bind:class="{ disabled: loading }" class="btn b-ok">
+        Login
+      </button>
     </form>
   </div>
 </template>
@@ -15,38 +30,46 @@
 <script>
 export default {
   inject: ["setToken"],
-  name: 'LoginView',
+  name: "LoginView",
   data() {
     return {
       username: "",
       password: "",
-      error: ""
-    }
+      error: "",
+      loading: false,
+    };
   },
   methods: {
     async login() {
-      let resp = await fetch("/api/login", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body : JSON.stringify({
-          username: this.username,
-          password: this.password
-        })
-      });
-      let { success, token, isAdmin, error } = await resp.json();
-      if (success) {
-        this.setToken(token, isAdmin);
-        this.$router.push({ name: "home" });
-      } else {
-        this.error = error;
+      try {
+        this.loading = true;
+        let resp = await fetch("/api/login", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            username: this.username,
+            password: this.password,
+          }),
+        });
+        let { success, token, isAdmin, error } = await resp.json();
+        if (success) {
+          this.setToken(token, isAdmin);
+          this.$router.push({ name: "home" });
+        } else {
+          this.error = error;
+        }
+        this.username = "";
+        this.password = "";
+        this.loading = false;
+      } catch (e) {
+        console.error(e);
+        this.loading = false;
       }
-      this.username = "";
-      this.password = "";
-    }
-  }
-}
+    },
+  },
+};
 </script>
 
 <style scoped>
@@ -55,5 +78,8 @@ export default {
   margin-top: 40px;
   width: 30%;
   background: grey;
+}
+.disabled {
+  background: rgb(145, 144, 144);
 }
 </style>

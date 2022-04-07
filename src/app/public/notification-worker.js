6148039b9b3254170e5382c9b6ba03ecae8642a4
Gabriele Morisano <gaballyno@gmail.com>
2022-04-07 12:32:36 +0200

    added some fixes to service worker

--- src/app/public/notification-worker.js
+++ src/app/public/notification-worker.js
@@ -1,11 +1,17 @@
-self.addEventListener("push", ev => {
-    const data = ev.data.json();
-    self.registration.showNotification(data.title, {
-      body: data.body,
-      icon: data.icon,
-      image: data.image,
-      vibrate: [300, 100, 400],
-    });
+self.addEventListener("install", (evt) => {
+  self.skipWaiting();
+});
+
+self.addEventListener("push", (ev) => {
+    self.registration.update();
+    const { title, body, icon, image } = ev.data.json();
+      ev.waitUntil(self.registration.showNotification(title, {
+        body: body,
+        icon: icon,
+        image: image,
+        vibrate: [300, 100, 400],
+      })
+    );
 });
 
 self.addEventListener("notificationclick", (event) => {
